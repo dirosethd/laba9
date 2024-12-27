b0747806d90c6645b9f5340fd6b8dc45298a8743
dirose <dirose@DESKTOP-PQA70RD>
2024-12-27 04:29:51 +0200

    Добавьте файлы проекта.

---
+++ laba9/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba9
+{
+    internal class Class1
+    {
+        public class Banknote
+        {
+            public int Denomination { get; private set; }
+            public int Quantity { get; private set; }
+            public Banknote(int denomination, int quantity)
+            {
+                if (denomination <= 0)
+                    throw new ArgumentException("Номинал купюры должен быть положительным.");
+                if (quantity < 0)
+                    throw new ArgumentException("Количество купюр не может быть отрицательным.");
+
+                Denomination = denomination;
+                Quantity = quantity;
+            }
+            public int CalculateTotalAmount()
+            {
+                return Denomination * Quantity;
+            }
+        }
+    }
+}
---
+++ laba9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static laba9.Class1;
+
+namespace  xd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Banknote banknote1 = new Banknote(10, 5);
+                Banknote banknote2 = new Banknote(5, 10);
+
+                int total1 = banknote1.CalculateTotalAmount();
+                int total2 = banknote2.CalculateTotalAmount();
+
+                Console.WriteLine($"Сумма {banknote1.Denomination} купюр: {total1}");
+                Console.WriteLine($"Сумма {banknote2.Denomination} купюр: {total2}");
+
+                int totalSum = total1 + total2;
+                Console.WriteLine($"Общая сумма: {totalSum}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
